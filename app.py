4f668dd6b46dc390d920c0bafd88ef4cc2ca2e70
Karthik5412 <kkuricharlapati@gmail.com>
2026-02-23 20:13:48 +0530

    Done

--- app.py
+++ app.py
@@ -1,3 +1,48 @@
 import streamlit as st
+from nltk.corpus import stopwords
+import string
+import joblib
 
-print('hi')
+clf = joblib.load('svm.plk')
+tfidf = joblib.load('tfidf.plk')
+
+stopwords = set(stopwords.words('english'))
+
+def remove_punc(txt) :
+    txt = txt.lower()
+    return txt.translate(str.maketrans('','',string.punctuation))
+
+def remove_num(txt):
+    new = ''
+    for i in txt :
+        if not i.isdigit():
+            new = new + i
+    return new
+
+def remove_emoji(txt):
+    new = ''
+    for i in txt :
+        if i.isascii():
+            new = new + i
+    return new
+
+st.title('Fake News Predictor')
+title = st.text_input('Enter title')
+text = st.text_input('Enter text')
+data = str(title) + str(text)
+
+final_data = tfidf.transform([data])
+btn = st.button('Find')
+
+
+if btn :
+    st.balloons()
+    pred = clf.predict(final_data)
+    if pred == 1:
+        st.success("This is not a fake news")
+    
+    else :
+        st.success("This is  a fake news")
+
+
+
